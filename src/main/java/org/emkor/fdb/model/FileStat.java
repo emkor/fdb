84532e68aee92901e0722db39288c67c2decdcf0
emkor93 <emkor93@gmail.com>
2018-07-06 11:50:42 +0200

    implemented tostring, hash, equals and getters for FileStat object

--- src/main/java/org/emkor/fdb/model/FileStat.java
+++ src/main/java/org/emkor/fdb/model/FileStat.java
@@ -1,6 +1,7 @@
 package org.emkor.fdb.model;
 
 import java.time.OffsetDateTime;
+import java.util.Objects;
 
 public class FileStat {
 
@@ -19,6 +20,46 @@ public class FileStat {
         this.change = change;
     }
 
+    public Long getSizeBytes() {
+        return sizeBytes;
+    }
 
+    public OffsetDateTime getAccess() {
+        return access;
+    }
 
+    public OffsetDateTime getModify() {
+        return modify;
+    }
+
+    public OffsetDateTime getChange() {
+        return change;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FileStat fileStat = (FileStat) o;
+        return Objects.equals(sizeBytes, fileStat.sizeBytes) &&
+                Objects.equals(access, fileStat.access) &&
+                Objects.equals(modify, fileStat.modify) &&
+                Objects.equals(change, fileStat.change);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(sizeBytes, access, modify, change);
+    }
+
+    @Override
+    public String toString() {
+        return "FileStat{" +
+                "sizeBytes=" + sizeBytes +
+                ", access=" + access +
+                ", modify=" + modify +
+                ", change=" + change +
+                '}';
+    }
 }
